abdf24dcc3aa0c94bc20815dcd208fdfa47a0dfc
pylover <vahid.mardani@gmail.com>
2021-02-18 00:38:57 +0330

    Cache for uns python command line, including a new command named find

--- uns.py
+++ uns.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env python3
-# pylama:ignore=D203,D102
-"""Simple IGMP utility."""
+# pylama:ignore=D203,D102,D107,E741,D105,E702,D103
+"""UNS utility."""
 
-
+import re
 import sys
 import socket
 import struct
+from os import path, environ
 
 import easycli as cli
 
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 # UNS verbs
@@ -21,15 +22,95 @@ VERB_ANSWER = 2
 GRP = '224.0.0.70'
 PORT = 5333
 TARGET = (GRP, PORT)
+DEFAULT_DBFILE = path.join(environ['HOME'], '.local', 'uns')
 
 
-def createsocket():
+IPPART_REGEX = r'\d{1,3}'
+IP_REGEX = r'.'.join([IPPART_REGEX] * 4)
+HOSTS_REGEX = r'[\w\s.]+'
+
+
+class DB:
+    """Simple database for UNS records."""
+
+    linepattern = re.compile(fr'({IP_REGEX})[\s\t]+({HOSTS_REGEX})')
+    ignorepattern = re.compile(r'^[\s#]')
+
+    def __init__(self, filename):
+        self.filename = filename
+        self._db = {}
+        self._names = {}
+
+    def append(self, addr, *hosts):
+        self._db[addr] = hosts
+        for h in hosts:
+            self._names[h] = addr
+
+    def parseline(self, l):
+        if self.ignorepattern.match(l):
+            return
+
+        m = self.linepattern.match(l)
+        if not m:
+            raise ValueError(f'Cannot parse: {l}')
+
+        addr, hosts = m.groups()
+        hosts = hosts.strip().split(' ')
+        self.append(addr, *hosts)
+
+    def save(self):
+        with open(self.filename, 'w') as f:
+            for addr, hosts in self._db.items():
+                f.write(f'{addr} {" ".join(hosts)}\n')
+
+    def resolve(self, hostname):
+        addr = self._names.get(hostname)
+        if not addr:
+            raise KeyError('Cannot find: %s', hostname)
+
+        return hostname, addr
+
+    def find(self, pattern):
+        for addr, hosts in self._db.items():
+            for h in hosts:
+                if h.startswith(pattern):
+                    yield h, addr
+
+    def __enter__(self):
+        if path.exists(self.filename):
+            with open(self.filename) as f:
+                for l in f:
+                    self.parseline(l)
+
+        return self
+
+    def __exit__(self, ex, extype, tb):
+        self.save()
+
+
+def createsocket(timeout=None):
     """Create a udp socket for IGMP multicast."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    if timeout:
+        sock.settimeout(timeout)
+
     return sock
+
+
+def readpacket(sock):
+    data, host = sock.recvfrom(256)
+    return data[0], data[1:].decode(), host[0], host[1]
+
+
+def printrecord(args, name, addr, cache):
+    if args.short:
+        print(addr)
+    else:
+        flags = ' [cache]' if cache else ''
+        print(f'{addr} {name}{flags}')
 
 
 class Answer(cli.SubCommand):
@@ -52,44 +133,95 @@ class Answer(cli.SubCommand):
         sock.sendto(verb + answer.encode(), (args.address, args.port))
 
 
-class Discover(cli.SubCommand):
-    """Discover command line interface."""
+class Find(cli.SubCommand):
+    """Find an ip address by it's name."""
 
-    __command__ = 'discover'
-    __aliases__ = ['d', 's']
+    __command__ = 'find'
+    __aliases__ = ['f']
     __arguments__ = [
-        cli.Argument('hostname'),
+        cli.Argument('pattern'),
+        cli.Argument('--no-cache', action='store_true'),
         cli.Argument('-s', '--short', action='store_true'),
-        cli.Argument('-w', '--wait', action='store_true',
-                     help='Wait for response'),
+        cli.Argument('-t', '--timeout', type=int, default=5,
+                     help='Seconds wait before exit, 0: infinite, default: 5'),
     ]
 
-    def print(self, args, data, host):
-        if args.short:
-            print(host[0])
-        else:
-            print(f'{":".join(map(str, host))}: {data.decode()}')
+    def online(self, db, args):
+        sock = createsocket(args.timeout)
+        discover = struct.pack('>B', VERB_DISCOVER)
+        sock.sendto(discover + args.pattern.encode(), TARGET)
+        try:
+            while True:
+                verb, name, addr, _ = readpacket(sock)
+                db.append(addr, name)
+                printrecord(args, name, addr, False)
+        except socket.timeout:
+            if not args.short:
+                print(f'Timeout reached: {args.timeout}', file=sys.stderr)
+                return
+        except KeyboardInterrupt:
+            print('Terminated by user.', file=sys.stderr)
 
     def __call__(self, args):
-        sock = createsocket()
+        with DB(args.dbfile) as db:
+            if not args.no_cache:
+                for name, addr in db.find(args.pattern):
+                    printrecord(args, name, addr, True)
+
+            self.online(db, args)
+
+
+class Resolve(cli.SubCommand):
+    """Resolve an ip address by it's name."""
+
+    __command__ = 'resolve'
+    __aliases__ = ['r', 'd']
+    __arguments__ = [
+        cli.Argument('hostname'),
+        cli.Argument('--no-cache', action='store_true'),
+        cli.Argument('-s', '--short', action='store_true'),
+        cli.Argument('-t', '--timeout', type=int, default=0,
+                     help='Wait for response, default: 0'),
+    ]
+
+    def online(self, db, args):
+        sock = createsocket(args.timeout)
+        discover = struct.pack('>B', VERB_DISCOVER)
         if not args.short:
             print(f'Sending {args.hostname} to {GRP}:{PORT}')
-        discover = struct.pack('>B', VERB_DISCOVER)
         sock.sendto(discover + args.hostname.encode(), TARGET)
-        if args.wait:
+        try:
             while True:
-                data, host = sock.recvfrom(256)
-                self.print(args, data, host)
-        else:
-            data, host = sock.recvfrom(256)
-            self.print(args, data, host)
+                verb, name, addr, _ = readpacket(sock)
+                db.append(addr, name)
+                if name == args.hostname:
+                    printrecord(args, name, addr, False)
+                    return
+        except socket.timeout:
+            if not args.short:
+                print(f'Timeout reached: {args.timeout}', file=sys.stderr)
+                return
+        except KeyboardInterrupt:
+            print('Terminated by user.', file=sys.stderr)
+
+    def __call__(self, args):
+        with DB(args.dbfile) as db:
+            if args.no_cache:
+                return self.online(db, args)
+
+            try:
+                name, addr = db.resolve(args.hostname)
+                printrecord(args, name, addr, True)
+
+            except KeyError:
+                return self.online(db, args)
 
 
-class Read(cli.SubCommand):
-    """Read IGMP packets."""
+class Sniff(cli.SubCommand):
+    """Sniff IGMP packets."""
 
-    __command__ = 'read'
-    __aliases__ = ['r']
+    __command__ = 'sniff'
+    __aliases__ = ['s']
     __arguments__ = [
     ]
 
@@ -99,9 +231,12 @@ class Read(cli.SubCommand):
         sock.bind(TARGET)
         mreq = struct.pack("4sl", socket.inet_aton(GRP), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        while True:
-            data, host = sock.recvfrom(256)
-            print(f'{host}: {data[1:].decode()}')
+        try:
+            while True:
+                verb, name, addr, port = readpacket(sock)
+                print(f'{addr}:{port} {name}')
+        except KeyboardInterrupt:
+            print('Terminated by user.', file=sys.stderr)
 
 
 class UNS(cli.Root):
@@ -111,9 +246,15 @@ class UNS(cli.Root):
     __help__ = 'UNS utility.'
     __arguments__ = [
         cli.Argument('-v', '--version', action='store_true'),
-        Discover,
-        Read,
+        cli.Argument(
+            '--dbfile',
+            default=DEFAULT_DBFILE,
+            help=f'Database file, default: {DEFAULT_DBFILE}'
+        ),
+        Resolve,
         Answer,
+        Sniff,
+        Find,
     ]
 
     def __call__(self, args):
